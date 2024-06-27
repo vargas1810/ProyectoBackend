80f0f3746fef6726da0585bb2684a3dda2153490
vargas1810 <jeffer.vargas.1810@gmail.com>
2024-06-27 01:18:27 -0500

    Mensaje descriptivo del commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:vargas1810@localhost/ProyectoSisvitaMax2'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://proyectosisvita_quix_user:qHhMun2POXm3cIM4LnmTQALrcc5gsgh3@dpg-cpufgclds78s73drp7vg-a.oregon-postgres.render.com/ProyectoSisvita'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
